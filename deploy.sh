e355c8c150147923480fdbd318bf7c52f3866b8a
Neo Ighodaro <neo@creativitykills.co>
2018-03-13 16:50:33 +0100

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -vuarz ~/Sites/tapsharp.com/* ck:/var/www/cydia.tapsharp.com/
---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="We make amazing mobile applications. A CreativityKills company.">
+    <link rel="icon" href="/assets/img/favicon.png">
+    <link rel="shortcut icon" href="/assets/img/favicon.png">
+    <title>TapSharp &mdash; Android &amp; iOS App Developers Based in Lagos, Nigeria</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/cydia.css" rel="stylesheet">
+</head>
+<body class="text-center">
+    <div class="wrapper">
+        <img src="/assets/img/logo.svg" alt="TapSharp Logo" class="logo">
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-6">
+                <a href="cydia://url/https://cydia.saurik.com/api/share#?source=http://cydia.tapsharp.com" class="btn custom">Add Repo to Cydia</a>
+            </div>
+            <div class="col col-lg-6">
+                <a href="https://twitter.com/neoighodaro" target="_blank" class="btn custom">Follow on Twitter</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
